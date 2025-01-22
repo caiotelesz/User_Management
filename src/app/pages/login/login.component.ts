b9247f16d0a09dc7c89fb007b387e454a4a7712a
caiotelesz <caioteles1267@gmail.com>
2025-01-22 14:27:08 -0300

    creating localStorage

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  userName: string;
+
+  constructor(private route: Router) {}
+
+  login() {
+    sessionStorage.setItem('user', this.userName);
+    
+
+    this.route.navigate(['home']);
+  }
 }

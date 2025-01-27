015226967984cef02c48cf351e2b91f48395c33b
caiotelesz <caioteles1267@gmail.com>
2025-01-26 22:55:19 -0300

    Creating interface

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+
+  nome: string = 'Caio Teles';
+
+  user: User = {
+    name: 'Caio Teles',
+    email: 'caio.teles@gmail.com',
+    sector: 'Tecnologia',
+    role: 'Desenvolvedor Front End Angular'
+  }
+
 
   constructor(private dataBaseStore: AngularFirestore) { }
 
@@ -15,12 +26,12 @@ export class UsersService {
   }
 
   // Add user
-  addUser(user: any) {
+  addUser(user: User) {
     return this.dataBaseStore.collection('users').add(user);
   }
 
   // Update user
-  updateUser(userId: string, user: any) {
+  updateUser(userId: string, user: User) {
     return this.dataBaseStore.collection('users').doc(userId).update(user);
   }
 
